9fc242b9dd3636d6eefb7067c176033b3ea2114c
Gian Michele Innocenti <ginnocen@cern.ch>
2024-11-13 11:36:29 +0100

    fix help message

--- MainAnalysis/20241113_gtoccbar/include/helpMessage.h
+++ MainAnalysis/20241113_gtoccbar/include/helpMessage.h
@@ -12,35 +12,14 @@ int printHelpMessage(int argc, char *argv[]) {
         if (arg == "--help") {
            std::cout << "Usage: " << argv[0] << " [OPTIONS]\n"
               << "Options:\n"
-              << "  --MinDPT <value>       Minimum D particle transverse momentum threshold for event selection.\n"
-              << "  --MaxDPT <value>       Maximum D particle transverse momentum threshold for event selection.\n"
-              << "  --MinDY <value>        Minimum D particle rapidity threshold for event selection.\n"
-              << "  --MaxDY <value>        Maximum D particle rapidity threshold for event selection.\n"
-/*
-              << "  --MinTrackPT <value>   Minimum track transverse momentum threshold for track selection.\n"
-              << "  --MaxTrackPT <value>   Maximum track transverse momentum threshold for track selection.\n"
-              << "  --MinHiBin <value>     Minimum hiBin value for event selection.\n"
-              << "  --MaxHiBin <value>     Maximum hiBin value for event selection.\n"
+              << "  --MinJetPT <value>     Minimum jet transverse momentum.\n"
               << "  --Input <path>         Path to the input ROOT file.\n"
-              << "  --MixFile <path>       Path to the mix ROOT file.\n"
               << "  --Output <path>        Path to the output ROOT file.\n"
-              << "  --IsSelfMixing         Flag to enable/disable self-mixing.\n"
-              << "  --IsGenZ               Flag to use generated level Z information.\n"
-              << "  --IsData               Flag to specify if the input is real data.\n"
-              << "  --IsPP                 Flag for Proton-Proton collision data.\n"
-              << "  --IsJewel              Flag for Jewel analysis.\n"
-              << "  --IsMuTagged           Flag for doing Z decay muon matching.\n"
-              << "  --IsPUReject           Flag to reject PU sample for systematics.\n"
               << "  --Fraction <value>     Fraction of event to be processed.\n"
               << "  --nThread <value>      Number of threads for parallel processing.\n"
               << "  --nChunk <value>       Specifies which chunk of the data to process.\n"
-              << "  --nMix <value>         Number of mixed events to be used.\n"
-              << "  --IsHiBinUp            Flag to use the hiBinUp value for event selection.\n"
-              << "  --IsHiBinDown          Flag to use the hiBinDown value for event selection.\n\n"
-              << "  --Shift                Shift of sumHF in MB matching\n\n"
-*/  
-            << "Example:\n"
-              << "  " << argv[0] << " --Input sample/HISingleMuon.root --Output output.root --MinZPT 20 --MaxZPT 100\n";
+              << "Example:\n"
+              << "  " << argv[0] << " --Input sample/HISingleMuon.root --Output output.root --MinJetPT 80 --Fraction 0.1 --nThread 4 --nChunk 0\n";
            return 1;
         }
     }
