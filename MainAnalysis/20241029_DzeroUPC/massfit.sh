b671d91f79d7ecf866ce82d984deef8fb1e99309
Janice Chen <janice.chen45@gmail.com>
2024-12-06 23:41:03 +0100

    [mod]

--- MainAnalysis/20241029_DzeroUPC/massfit.sh
+++ MainAnalysis/20241029_DzeroUPC/massfit.sh
@@ -8,6 +8,8 @@ jq -c '.MicroTrees[]' $FitSettingCard | while read MicroTree; do
 	sigswpInput=$(echo $MicroTree | jq -r '.sigswpInput')
 	KKmcInput=$(echo $MicroTree | jq -r '.KKmcInput')
 	pipimcInput=$(echo $MicroTree | jq -r '.pipimcInput')
+	neventsInput=$(echo $MicroTree | jq -r '.neventsInput')
+	doSyst_comb=$(echo $MicroTree | jq -r '.doSyst_comb')
 	RstDir=$(dirname "$dataInput")
 	RstDir=${RstDir}/${FitDir}/
 	mkdir -p $RstDir
@@ -18,6 +20,8 @@ jq -c '.MicroTrees[]' $FitSettingCard | while read MicroTree; do
   [ "$sigswpInput" != "null" ] && cmd="$cmd --sigswpInput $sigswpInput"
   [ "$KKmcInput" != "null" ] && cmd="$cmd --KKmcInput $KKmcInput"
   [ "$pipimcInput" != "null" ] && cmd="$cmd --pipimcInput $pipimcInput"
+  [ "$neventsInput" != "null" ] && cmd="$cmd --neventsInput $neventsInput"
+  [ "$doSyst_comb" != "null" ] && cmd="$cmd --doSyst_comb $doSyst_comb"
   cmd="$cmd --Output fit.root --RstDir $RstDir"
 
   # Execute the command and log output
