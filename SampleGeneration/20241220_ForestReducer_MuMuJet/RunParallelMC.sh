71dd458e396bd7a5aebb22185740835f292add14
Gian Michele Innocenti <ginnocen@cern.ch>
2024-12-22 02:18:41 +0100

    update MC

--- SampleGeneration/20241220_ForestReducer_MuMuJet/RunParallelMC.sh
+++ SampleGeneration/20241220_ForestReducer_MuMuJet/RunParallelMC.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 MAXCORES=120
+SAMPLEID=0
 
-NAME="SkimMC2018PbPb_Version20241201_InputForest_20241201_DiJet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8_PARTIAL"
-OUTPUT="output"
+echo "Running on sample ID: $SAMPLEID"
+
+if [ "$SAMPLEID" -eq 0 ]; then
+    NAMEMC="SkimMC2018PbPb_Version20241220_InputForest_20241217_DiJet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8"
+    FOLDER="/data00/g2ccbar/mc2018/forest_12172024"
+fi
+echo "Running on sample: $NAMEMC"
+echo "Running on folder: $FOLDER"
+
+OUTPUTMC="outputMC"
 counter=0
-filelist="/data/NewSkims_gtoccbar/InputList/20241201_DiJet_pThat-15_TuneCP5_HydjetDrumMB_5p02TeV_Pythia8_PARTIAL.txt"
-MERGEDOUTPUT="/data/NewSkims_gtoccbar/$NAME.root"
-rm $MERGEDOUTPUT
+filelistMC="filelist.txt"
+MERGEDOUTPUTMC="$NAMEMC"
+MERGEDOUTPUTMCFILE="$NAMEMC/mergedfile.root"
+
+rm -rf $MERGEDOUTPUTMC
+mkdir $MERGEDOUTPUTMC
+cp ./RunParallelMC.sh $MERGEDOUTPUTMC/.
 
 # Function to monitor active processes
 wait_for_slot() {
@@ -16,29 +29,31 @@ wait_for_slot() {
     done
 }
 
+ls $FOLDER/HiForestMiniAOD_*.root > $filelistMC
+echo "File list created successfully: $filelistMC"
 
 # Check if the filelist is empty
-if [[ ! -s "$filelist" ]]; then
+if [[ ! -s "$filelistMC" ]]; then
     echo "No matching files found in Samples directory."
     exit 1
 fi
 
-echo "File list created successfully: $filelist"
-rm -rf $OUTPUT
-mkdir $OUTPUT
+echo "File list created successfully: $filelistMC"
+rm -rf $OUTPUTMC
+mkdir $OUTPUTMC
 # Loop through each file in the file list
 while IFS= read -r file; do
             echo "Processing $file"
             ./Execute --Input "$file" \
             --IsData false \
             --IsPP false \
-            --Output "$OUTPUT/output_$counter.root" \
-            --MinJetPT 40 --Fraction 1.0 & 
+            --Output "$OUTPUTMC/output_$counter.root" \
+            --MinJetPT 80 --Fraction 1.0 & 
     ((counter++))
     wait_for_slot
-done < "$filelist"
+done < "$filelistMC"
 wait 
 
-hadd $MERGEDOUTPUT $OUTPUT/output_*.root
-echo "All done!"
-echo "Merged output file: $MERGEDOUTPUT"
+hadd $MERGEDOUTPUTMCFILE $OUTPUTMC/output_*.root
+echo "All done MC!"
+echo "Merged output file: $MERGEDOUTPUTMCFILE"
