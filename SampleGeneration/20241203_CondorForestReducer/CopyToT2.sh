f032519d560afdec46a5a6f9036ac555f490de5e
Jordan Lang <jdlang@mit.edu>
2025-01-13 14:43:43 -0500

    Removed duplicate scripts from DZeroUPC_2023OldReco dir

--- SampleGeneration/20241203_CondorForestReducer/CopyToT2.sh
+++ SampleGeneration/20241203_CondorForestReducer/CopyToT2.sh
@@ -2,6 +2,7 @@
 
 SAMPLEGEN_DIRS=(
   '20241214_ForestReducer_DzeroUPC_2023OldReco'
+  '20241204_ForestReducer_DzeroUPC_2024PromptReco'
   '20241220_ForestReducer_MuMuJet'
 )
 
@@ -22,7 +23,7 @@ xrdcp -f ../../SetupAnalysis.sh root://xrootd.cmsaf.mit.edu//store/user/$USER/MI
 for SAMPLEGEN_DIR in ${SAMPLEGEN_DIRS[@]}; do
   # Copy over current state of Condor dir
   xrdfs root://xrootd.cmsaf.mit.edu/ mkdir -p /store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR
-  xrdcp -r -f include       	root://xrootd.cmsaf.mit.edu//store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR/
-  xrdcp -f ReduceForest.cpp 	root://xrootd.cmsaf.mit.edu//store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR/
-  xrdcp -f makefile         	root://xrootd.cmsaf.mit.edu//store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR/
+  xrdcp -r -f $ProjectBase/SampleGeneration/$SAMPLEGEN_DIR/include       	root://xrootd.cmsaf.mit.edu//store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR/
+  xrdcp -f $ProjectBase/SampleGeneration/$SAMPLEGEN_DIR/ReduceForest.cpp 	root://xrootd.cmsaf.mit.edu//store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR/
+  xrdcp -f $ProjectBase/SampleGeneration/$SAMPLEGEN_DIR/makefile         	root://xrootd.cmsaf.mit.edu//store/user/$USER/MITHIGAnalysis2024/SampleGeneration/$SAMPLEGEN_DIR/
 done
--- SampleGeneration/20241214_ForestReducer_DzeroUPC_2023OldReco/RunCondorSkim.sh
+++ SampleGeneration/20241214_ForestReducer_DzeroUPC_2023OldReco/RunCondorSkim.sh
@@ -3,15 +3,17 @@
 # Source of forest files (use xrootd path)
 SOURCE_SERVER="root://xrootd.cmsaf.mit.edu/"
 SOURCE_DIR="/store/user/jdlang/run3_2023Data_Jan2024ReReco"
-
 # Output of skimmed files (use xrootd path)
 OUTPUT_SERVER="root://xrootd.cmsaf.mit.edu/"
 OUTPUT_DIR="/store/user/$USER/run3_2023Data_Jan2024ReReco_Skims_20250113"
 
-# Local directory for condor configs
-DATE=$(date +%Y%m%d)
-CONFIG_DIR="condorSkimConfigs_${DATE}"
-MASTER_FILE_LIST="${CONFIG_DIR}/forestFilesForSkim.txt"
+# Job settings (memory and storage are in GB)
+FILES_PER_JOB=200
+JOB_MEMORY=5
+JOB_STORAGE=20
+CMSSW_VERSION="CMSSW_13_2_4"
+# Subdirectory within MITHIGAnalysis2024 with skim scripts
+ANALYSIS_SUBDIR='SampleGeneration/20241214_ForestReducer_DzeroUPC_2023OldReco/'
 
 # Job settings (memory and storage are in GB)
 FILES_PER_JOB=200
@@ -23,7 +25,7 @@ ANALYSIS_SUBDIR='SampleGeneration/20241214_ForestReducer_DzeroUPC_2023OldReco/'
 
 # Includes VOMS proxy in process
 REFRESH_PROXY=0
-# Copies key scripts from MITHIGAnalysis to T2_US_MIT for compiler
+# Copy key scripts from MITHIGAnalysis to T2_US_MIT for compiler
 COPY_TO_T2=0
 
 if [[ $REFRESH_PROXY -eq 1 ]]; then
@@ -32,12 +34,13 @@ if [[ $REFRESH_PROXY -eq 1 ]]; then
   export PROXYFILE=~/x509up_u$(id -u)
 fi
 if [[ $COPY_TO_T2 -eq 1 ]]; then
-  ./CopyToT2.sh
+  ../20241203_CondorForestReducer/CopyToT2.sh
   wait
 fi
 xrdfs $OUTPUT_SERVER mkdir -p $OUTPUT_DIR
 mkdir -p $CONFIG_DIR
-./MakeXrdFileList.sh $SOURCE_SERVER $SOURCE_DIR $MASTER_FILE_LIST
+../20241203_CondorForestReducer/MakeXrdFileList.sh $SOURCE_SERVER $SOURCE_DIR \
+  $MASTER_FILE_LIST
 
 # Function for job submission
 submit_condor_jobs() {
@@ -45,9 +48,9 @@ submit_condor_jobs() {
   local JOB_LIST=$2
   local JOB_COUNTER=$3
   OUTPUT_PATH="${OUTPUT_DIR}/skim_output_${JOB_COUNTER}.root"
-  ./MakeCondorSkim.sh $BASENAME $JOB_LIST $CONFIG_DIR $OUTPUT_SERVER \
-    $OUTPUT_PATH $PROXYFILE $JOB_MEMORY $JOB_STORAGE $CMSSW_VERSION
-    $ANALYSIS_SUBDIR
+  ../20241203_CondorForestReducer/MakeCondorSkim.sh $BASENAME $JOB_LIST \
+    $CONFIG_DIR $OUTPUT_SERVER $OUTPUT_PATH $PROXYFILE $JOB_MEMORY \
+    $JOB_STORAGE $CMSSW_VERSION $ANALYSIS_SUBDIR
   wait
   sleep 0.5
   return 0
