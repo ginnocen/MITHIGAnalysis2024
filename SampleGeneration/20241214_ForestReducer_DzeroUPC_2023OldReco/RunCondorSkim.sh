cfc90eb2bcb8cb0f11cb5648d99462101294d388
Jordan Lang <jdlang@mit.edu>
2025-01-13 00:55:08 -0500

    Separate xrootd servers for source and output

--- SampleGeneration/20241214_ForestReducer_DzeroUPC_2023OldReco/RunCondorSkim.sh
+++ SampleGeneration/20241214_ForestReducer_DzeroUPC_2023OldReco/RunCondorSkim.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-DATE=$(date +%Y%m%d)
-XROOTD_SERVER="root://xrootd.cmsaf.mit.edu/"
+SOURCE_SERVER="root://xrootd.cmsaf.mit.edu/"
 SOURCE_DIR="/store/user/jdlang/run3_2023Data_Jan2024ReReco"
+OUTPUT_SERVER="root://xrootd.cmsaf.mit.edu/"
 OUTPUT_DIR="/store/user/jdlang/run3_2023Data_Jan2024ReReco_Skims_20250108"
+
+DATE=$(date +%Y%m%d)
 CONFIG_DIR="condorSkimConfigs_${DATE}"
 MASTER_FILE_LIST="${CONFIG_DIR}/forestFilesForSkim.txt"
 FILES_PER_JOB=200
+# Resource request in GB:
+JOB_MEMORY=5
+JOB_STORAGE=10
 
 # Includes VOMS proxy in process
-REFRESH_PROXY=0
+REFRESH_PROXY=1
 # Copies key scripts from MITHIGAnalysis to T2_US_MIT for compiler
-COPY_TO_T2=0
+COPY_TO_T2=1
 
 if [[ $REFRESH_PROXY -eq 1 ]]; then
   voms-proxy-init -rfc -voms cms -valid 120:00
@@ -22,9 +27,9 @@ if [[ $COPY_TO_T2 -eq 1 ]]; then
   ./CopyToT2.sh
   wait
 fi
-xrdfs $XROOTD_SERVER mkdir -p $OUTPUT_DIR
+xrdfs $OUTPUT_SERVER mkdir -p $OUTPUT_DIR
 mkdir -p $CONFIG_DIR
-./MakeXrdFileList.sh $XROOTD_SERVER $SOURCE_DIR $MASTER_FILE_LIST
+./MakeXrdFileList.sh $SOURCE_SERVER $SOURCE_DIR $MASTER_FILE_LIST
 
 # Function for job submission
 submit_condor_jobs() {
@@ -32,7 +37,7 @@ submit_condor_jobs() {
   local JOB_LIST=$2
   local JOB_COUNTER=$3
   OUTPUT_PATH="${OUTPUT_DIR}/skim_output_${JOB_COUNTER}.root"
-  ./MakeCondorSkim.sh $BASENAME $JOB_LIST $CONFIG_DIR $XROOTD_SERVER $OUTPUT_PATH $PROXYFILE
+  ./MakeCondorSkim.sh $BASENAME $JOB_LIST $CONFIG_DIR $OUTPUT_SERVER $OUTPUT_PATH $PROXYFILE $JOB_MEMORY $JOB_STORAGE
   wait
   sleep 0.5
   return 0
