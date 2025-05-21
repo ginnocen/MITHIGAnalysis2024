f398fc9c781fa3c377fa8e1871392306c983c992
Gian Michele Innocenti <ginnocen@cern.ch>
2025-05-20 21:22:38 -0400

    adding leading pT particle info

--- SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples.sh
+++ SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples.sh
@@ -5,14 +5,14 @@ rm -rf Output/
 mkdir -p Output/
 ./Execute --Input ${PATHSAMPLE}/HiForestMiniAOD_Hijing_merged.root \
    --Output skim_HiForest_Hijing.root \
-   --DoGenLevel true \
+   --DoGenLevel false \
    --Year 2025 \
    --IsData false \
    --PFTree particleFlowAnalyser/pftree
 
 ./Execute --Input ${PATHSAMPLE}/crab_HiForest_250514_Starlight_SingleDiffraction_OO_5362GeV_1505PR47944.root \
    --Output skim_HiForest_SingleDiffraction.root \
-   --DoGenLevel true \
+   --DoGenLevel false \
    --Year 2025 \
    --IsData false \
    --PFTree particleFlowAnalyser/pftree
