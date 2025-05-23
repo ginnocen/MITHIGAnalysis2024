90fac7b02d8b5bf5ca8b51fe0492f9063d34a451
Gian Michele Innocenti <ginnocen@cern.ch>
2025-05-22 21:36:21 -0400

    update bash files

--- SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples.sh
+++ SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/RunExamples.sh
@@ -3,15 +3,15 @@ source clean.sh
 
 rm -rf Output/
 mkdir -p Output/
-./Execute --Input ${PATHSAMPLE}/HiForestMiniAOD_Hijing_merged.root \
-   --Output skim_HiForest_Hijing.root \
+./Execute --Input ${PATHSAMPLE}/crab_HiForest_250520_Hijing_MinimumBias_b015_OO_5362GeV_250518.root \
+   --Output ${PATHSKIMSAMPLE}/skim_HiForest_250520_Hijing_MinimumBias_b015_OO_5362GeV_250518.root \
    --DoGenLevel false \
    --Year 2025 \
    --IsData false \
    --PFTree particleFlowAnalyser/pftree
 
 ./Execute --Input ${PATHSAMPLE}/crab_HiForest_250514_Starlight_SingleDiffraction_OO_5362GeV_1505PR47944.root \
-   --Output skim_HiForest_SingleDiffraction.root \
+   --Output ${PATHSKIMSAMPLE}/skim_HiForest_250514_Starlight_SingleDiffraction_OO_5362GeV_1505PR47944.root \
    --DoGenLevel false \
    --Year 2025 \
    --IsData false \
--- SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/clean.sh
+++ SampleGeneration/20250518_ForestReducer_RAAOxygenOxygen/clean.sh
@@ -1,7 +1,8 @@
 USEONMITHI03=false
 CMSFOLDER=/home/$USER/CMSSW_13_2_4/src
 CMSSUBMIT=/home/submit/$USER/CMSSW_14_1_7/src
-export PATHSAMPLE="data00/OOsamples"
+export PATHSAMPLE="data00/OOsamples/Forests"
+export PATHSKIMSAMPLE="data00/OOsamples/Skims"
 
 if [ -d "$CMSSUBMIT" ]; then
     CMSFOLDER=$CMSSUBMIT
