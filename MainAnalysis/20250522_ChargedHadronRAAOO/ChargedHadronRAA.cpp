575a58a09c7b650e2b9fb68248c0dea6f93b1f3c
Gian Michele Innocenti <ginnocen@cern.ch>
2025-05-28 16:13:03 -0400

    skeleton of the OO RAA analysis

---
+++ MainAnalysis/20250522_ChargedHadronRAAOO/ChargedHadronRAA.cpp
@@ -0,0 +1,103 @@
+#include <TCanvas.h>
+#include <TCut.h>
+#include <TFile.h>
+#include <TH1D.h>
+#include <TH2D.h>
+#include <TH3D.h>
+#include <TLegend.h>
+#include <TNtuple.h>
+#include <TTree.h>
+
+#include <iostream>
+
+using namespace std;
+#include "CommandLine.h"
+#include "Messenger.h"
+#include "ProgressBar.h"
+#include "helpMessage.h"
+#include "parameter.h" // Parameters for the analysis
+#include "utilities.h" // Utility functions for the analysis
+
+//============================================================//
+// Function to check for configuration errors
+//============================================================//
+bool checkError(const Parameters &par) { return false; }
+
+class DataAnalyzer {
+public:
+  TFile *inf, *outf;
+  TH1D *hTrkPt;
+  ChargedHadronRAATreeMessenger *MChargedHadronRAA;
+  TNtuple *nt;
+  string title;
+
+  DataAnalyzer(const char *filename, const char *outFilename, const char *mytitle = "")
+      : inf(new TFile(filename)), MChargedHadronRAA(new ChargedHadronRAATreeMessenger(*inf, string("Tree"))),
+        title(mytitle), outf(new TFile(outFilename, "recreate")) {
+    outf->cd();
+  }
+
+  ~DataAnalyzer() {
+    deleteHistograms();
+    inf->Close();
+    outf->Close();
+    delete MChargedHadronRAA;
+  }
+
+  void analyze(Parameters &par) {
+    outf->cd();
+    hTrkPt = new TH1D(Form("hTrkPt%s", title.c_str()), "", 100, 0, 10);
+    hTrkPt->Sumw2();
+    par.printParameters();
+    unsigned long nEntry = MChargedHadronRAA->GetEntries() * par.scaleFactor;
+    ProgressBar Bar(cout, nEntry);
+    Bar.SetStyle(1);
+
+    for (unsigned long i = 0; i < nEntry; i++) {
+      MChargedHadronRAA->GetEntry(i);
+      if (i % 1000 == 0) {
+        Bar.Update(i);
+        Bar.Print();
+        for (unsigned long j = 0; j < MChargedHadronRAA->trkPt->size(); j++) {
+          hTrkPt->Fill(MChargedHadronRAA->trkPt->at(j));
+        }
+      }
+    } // end of event loop
+  } // end of analyze
+
+  void writeHistograms(TFile *outf) {
+    outf->cd();
+    smartWrite(hTrkPt);
+    smartWrite(nt);
+  }
+
+private:
+  void deleteHistograms() { delete hTrkPt; }
+};
+
+//============================================================//
+// Main analysis
+//============================================================//
+int main(int argc, char *argv[]) {
+  if (printHelpMessage(argc, argv))
+    return 0;
+  CommandLine CL(argc, argv);
+  float MinTrackPt = CL.GetDouble("MinTrackPt", 1.0);   // Minimum track pT selection
+  bool IsData = CL.GetBool("IsData", 0); // Data or MC
+  float scaleFactor = CL.GetDouble("ScaleFactor", 1.0);
+  int TriggerChoice = CL.GetInt("TriggerChoice", 0);
+  Parameters par(MinTrackPt, TriggerChoice, IsData, scaleFactor);
+  par.input = CL.Get("Input", "input.root");    // Input file
+  par.output = CL.Get("Output", "output.root"); // Output file
+  if (checkError(par))
+    return -1;
+  std::cout << "Parameters are set" << std::endl;
+  // Analyze Data
+  DataAnalyzer analyzer(par.input.c_str(), par.output.c_str(), "");
+  analyzer.analyze(par);
+  analyzer.writeHistograms(analyzer.outf);
+  saveParametersToHistograms(par, analyzer.outf);
+  cout << "done!" << analyzer.outf->GetName() << endl;
+
+  return 0;
+}
