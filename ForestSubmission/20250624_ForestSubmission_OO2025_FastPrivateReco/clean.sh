9eec80d48269dc5d84b81f9456f85e09fbb2edd7
Jordan Lang <jordandlang.sci@gmail.com>
2025-06-25 18:06:53 +0200

    First version of forest configs for OO

---
+++ ForestSubmission/20250624_ForestSubmission_OO2025_FastPrivateReco/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ">>> Setting CMSSW environment"
+cd ~/CMSSW_15_0_*/src
+cmsenv
+cd -
---
+++ ForestSubmission/20250624_ForestSubmission_OO2025_FastPrivateReco/forest_CRABConfig_Run3_OO_DATA.py
@@ -0,0 +1,38 @@
+from CRABClient.UserUtilities import config
+from CRABClient.UserUtilities import getUsername
+username = getUsername()
+
+###############################################################################
+# INPUT/OUTPUT SETTINGS
+
+jobTag = 'Run3_OO_TestDataConfigsWithMC_v1'
+input = 'DAS_PATH'
+inputDatabase = 'global'
+output = '/store/group/phys_heavyions/' + username + '/Run3_OO_2025Data_FastPrivateReco/'
+
+###############################################################################
+
+config = Configuration()
+
+config.General.requestName = jobTag
+config.General.workArea = 'CrabWorkArea'
+config.General.transferOutputs = True
+
+config.JobType.psetName = 'forest_CMSSWConfig_Run3_OO_DATA_miniAOD.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.maxMemoryMB = 5000
+config.JobType.pyCfgParams = ['noprint']
+#config.JobType.scriptExe = 'ZDCemapScript.sh'
+#config.JobType.inputFiles = ['ZDCemap_PbPbUPC2023.txt']
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = input
+config.Data.inputDBS = inputDatabase
+config.Data.outLFNDirBase = output
+config.Data.splitting = 'EventAwareLumiBased'
+config.Data.unitsPerJob = 10000
+config.Data.totalUnits = -1
+config.Data.publication = False
+config.Data.allowNonValidInputDataset = True
+
+config.Site.storageSite = 'T2_CH_CERN'
