b6b05777f4d59c609b9aa226062d99760f2ade6d
Jordan Lang <jordandlang.sci@gmail.com>
2025-06-26 16:23:53 +0200

    Fixes to CRAB config

--- ForestSubmission/20250624_ForestSubmission_OO2025_FastPrivateReco/forest_CRABConfig_Run3_OO_DATA.py
+++ ForestSubmission/20250624_ForestSubmission_OO2025_FastPrivateReco/forest_CRABConfig_Run3_OO_DATA.py
@@ -6,14 +6,15 @@ username = getUsername()
 # INPUT/OUTPUT SETTINGS
 
 jobTag = 'Run3_OO_IonPhysics_runXXXXXX'
-input = 'DAS_PATH'
-inputDatabase = 'global'
+input = '/Hijing_MinimumBias_b015_OO_5362GeV/wangj-MINIAOD_250518_el8_Run3_2025_OXY_1506-2f28ed489918bb46492162f28bc838f0/USER'
+#inputDatabase = 'global'
+inputDatabase = 'phys03'
 output = '/store/group/phys_heavyions/' + username + '/Run3_OO_2025Data_FastPrivateReco/'
 outputServer = 'T2_CH_CERN'
 
 ###############################################################################
 
-config = Configuration()
+config = config()
 
 config.General.requestName = jobTag
 config.General.workArea = 'CrabWorkArea'
@@ -32,7 +33,7 @@ config.Data.inputDBS = inputDatabase
 config.Data.outLFNDirBase = output
 config.Data.splitting = 'EventAwareLumiBased'
 config.Data.unitsPerJob = 10000
-config.Data.totalUnits = -1
+config.Data.totalUnits = 1
 config.Data.publication = False
 config.Data.allowNonValidInputDataset = True
 
