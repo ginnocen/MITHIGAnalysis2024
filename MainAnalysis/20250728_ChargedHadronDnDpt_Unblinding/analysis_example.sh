7f23385bd83c2d16e48efa7375696e1c81383ab5
Cristian Baldenegro <Cristian-Baldenegro@users.noreply.github.com>
2025-07-29 15:27:15 -0400

    Update analysis_example.sh

--- MainAnalysis/20250728_ChargedHadronDnDpt_Unblinding/analysis_example.sh
+++ MainAnalysis/20250728_ChargedHadronDnDpt_Unblinding/analysis_example.sh
@@ -7,19 +7,21 @@ source clean.sh
 
 INPUT=/data00/kdeverea/OOsamples/Skims/output_20250728_Skim_OO_IonPhysics0_LowPtV2_250711_104114/0000_merged.root
 #INPUT=samples/skim_HiForest_2025OO_LowPtCfg_2210.root
-OUTPUTANALYSIS=output/20250728_skim_HiForest_2025OO_LowPtCfg_2210.root
+OUTPUTANALYSIS=output/20250728_skim_HiForest_2025OO_LowPtCfg_2210_trackTight.root
 
-./ExecuteChargedHadrondNdpT \
+./ExecuteChargedHadrondNdpt \
   --Input $INPUT \
   --Output $OUTPUTANALYSIS \
   --IsData true \
-  --IsPP true \
   --ApplyEventSelection true \
   --UseEventWeight false \
   --UseTrackWeight false \
-  --TrackWeightSelection 2 \
+  --TrackWeightSelection 1 \
   --MinTrackPt 0.1 \
   --MinLeadingTrackPt -1 \
+  --TriggerChoice 1 \
+  --CollisionSystem true \
+  --EventSelectionOption 0 \
   --ScaleFactor 1
 
 # 1: loose , 2: nominal , 3: tight , 4: y2017
