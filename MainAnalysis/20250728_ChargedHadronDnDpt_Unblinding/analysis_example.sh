669dbfb03682837715ae20920687c6f1031d980a
Cristian Baldenegro <Cristian-Baldenegro@users.noreply.github.com>
2025-08-04 13:42:16 -0400

    main code update (#172)
    * small fixes to main analysis
    
    * Update analysis_example.sh
    
    * Update makefile
    
    * Update ChargedHadrondNdpT.cpp
    
    * Update analysis_example.sh
    
    * Update ChargedHadrondNdpT.cpp
    
    * Update parameter.h
    
    * Update analysis_example.sh

--- MainAnalysis/20250728_ChargedHadronDnDpt_Unblinding/analysis_example.sh
+++ MainAnalysis/20250728_ChargedHadronDnDpt_Unblinding/analysis_example.sh
@@ -5,21 +5,26 @@
 # ============================================================
 source clean.sh
 
-INPUT=/data00/kdeverea/OOsamples/Skims/output_20250728_Skim_OO_IonPhysics0_LowPtV2_250711_104114/0000_merged.root
+INPUT=/data00/OOsamples/Skims/tempOO.root
+#/data00/kdeverea/OOsamples/Skims/output_20250728_Skim_OO_IonPhysics0_LowPtV2_250711_104114/0000_merged.root
 #INPUT=samples/skim_HiForest_2025OO_LowPtCfg_2210.root
-OUTPUTANALYSIS=output/20250728_skim_HiForest_2025OO_LowPtCfg_2210.root
+OUTPUTANALYSIS=output/20250728_skim_HiForest_2025OO_LowPtCfg_2210_trackTight.root
 
-./ExecuteChargedHadrondNdpT \
+./ExecuteChargedHadrondNdpt \
   --Input $INPUT \
   --Output $OUTPUTANALYSIS \
   --IsData true \
-  --IsPP true \
   --ApplyEventSelection true \
+  --UseSpeciesWeight false \
   --UseEventWeight false \
   --UseTrackWeight false \
-  --TrackWeightSelection 2 \
-  --MinTrackPt 0.1 \
+  --TrackWeightSelection 3 \
+  --MinTrackPt 0.4 \
   --MinLeadingTrackPt -1 \
+  --TriggerChoice 1 \
+  --CollisionSystem true \
+  --EventSelectionOption 0 \
+  --SpeciesCorrectionOption 0 \
   --ScaleFactor 1
 
 # 1: loose , 2: nominal , 3: tight , 4: y2017
