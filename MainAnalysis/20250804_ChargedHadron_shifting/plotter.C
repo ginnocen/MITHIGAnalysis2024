11070002248d83daa3ff7697999849a46d6bba94
Abraham Holtermann <ath8@mit.edu>
2025-08-05 18:51:59 +0200

    add ratio plot

--- MainAnalysis/20250804_ChargedHadron_shifting/plotter.C
+++ MainAnalysis/20250804_ChargedHadron_shifting/plotter.C
@@ -40,21 +40,42 @@ void plotter(){
     mc_hfe->SetLineWidth(2);
     shiftedD->SetLineWidth(2);
 
-    // Create canvas with CMS style
-    TCanvas* c1 = new TCanvas("c1", "Canvas", 800, 600);
-    TPad* pad = (TPad*)c1->GetPad(0);
+    // Create canvas with CMS style - taller for ratio plot
+    TCanvas* c1 = new TCanvas("c1", "Canvas", 800, 800);
     
-    // Set pad margins
-    pad->SetMargin(marginLeft, marginRight, marginBottom, marginTop);
+    // Create upper pad for main plot
+    TPad* pad1 = new TPad("pad1", "pad1", 0, 0.3, 1, 1.0);
+    pad1->SetBottomMargin(0.02);
+    pad1->SetMargin(marginLeft, marginRight, 0.02, marginTop);
+    pad1->SetLogy();
+    pad1->SetLogx();
+    pad1->SetGrid();
+    pad1->Draw();
     
-    c1->SetLogy();
-    c1->SetLogx();
-    c1->SetGrid();
+    // Create lower pad for ratio plot
+    TPad* pad2 = new TPad("pad2", "pad2", 0, 0.0, 1, 0.3);
+    pad2->SetTopMargin(0.02);
+    pad2->SetBottomMargin(0.35);
+    pad2->SetMargin(marginLeft, marginRight, 0.35, 0.02);
+    pad2->SetLogx();
+    pad2->SetGrid();
+    pad2->Draw();
+    
+    // Upper plot
+    pad1->cd();
     
     // Set fonts for histograms
     SetTH1Fonts(data_hfe);
     SetTH1Fonts(mc_hfe);
     SetTH1Fonts(shiftedD);
+    
+    // Increase y-axis label font size for upper plot
+    data_hfe->GetYaxis()->SetLabelSize(0.05);
+    data_hfe->GetYaxis()->SetTitleSize(0.06);
+    
+    // Remove x-axis title and labels from upper plot
+    data_hfe->GetXaxis()->SetLabelSize(0);
+    data_hfe->GetXaxis()->SetTitle("");
     
     data_hfe->Draw("lpe");
     mc_hfe->Draw("HIST SAME");
@@ -73,8 +94,8 @@ void plotter(){
     legend->Draw();
 
     // Add CMS headers
-    AddCMSHeader(pad, "Preliminary");
-    AddUPCHeader(pad, "5.36 TeV", "NeNe 1 nb^{-1}");
+    AddCMSHeader(pad1, "Preliminary");
+    AddUPCHeader(pad1, "5.36 TeV", "NeNe 1 nb^{-1}");
     
     // Add fit information
     std::vector<TString> fitInfo;
@@ -101,7 +122,7 @@ void plotter(){
     // Add abbreviated description
     fitInfo.push_back("");
     
-    AddPlotLabels(pad, fitInfo, plotTextSize*0.85, plotTextOffset, 0.22, 0.25);
+    AddPlotLabels(pad1, fitInfo, plotTextSize*0.85, plotTextOffset, 0.22, 0.18);
     
     // Add event selection information
     std::vector<TString> eventSelInfo;
@@ -109,11 +130,53 @@ void plotter(){
     eventSelInfo.push_back("PV + CC Filters, |V_{z}| < 15 cm");
     eventSelInfo.push_back("HLT MinBias (Data only)");
     
-    AddPlotLabels(pad, eventSelInfo, plotTextSize*0.80, plotTextOffset, 0.2, 0.78);
+    AddPlotLabels(pad1, eventSelInfo, plotTextSize*0.80, plotTextOffset, 0.2, 0.77);
     
-    // Add plot label
-    //AddPlotLabel(pad, "HFE Max  Distributions");
-
+    // Create ratio plots
+    pad2->cd();
+    
+    // Create ratio histogram for fitted spectrum only (shifted/data)
+    TH1D* ratio_fitted = (TH1D*)shiftedD->Clone("ratio_fitted");
+    
+    // Calculate ratio (Shifted/Data)
+    ratio_fitted->Divide(data_hfe);
+    
+    // Set up ratio plot styling
+    ratio_fitted->SetTitle("");
+    ratio_fitted->GetXaxis()->SetTitle("HFE Max AND (GeV)");
+    ratio_fitted->GetYaxis()->SetTitle("Ratio");
+    ratio_fitted->GetXaxis()->SetRangeUser(0, 600);
+    ratio_fitted->SetMinimum(0.5);
+    ratio_fitted->SetMaximum(1.5);
+    
+    // Style the ratio plot in black with lpe
+    ratio_fitted->SetLineColor(kBlack);
+    ratio_fitted->SetMarkerColor(kBlack);
+    ratio_fitted->SetMarkerStyle(mCircleFill);
+    ratio_fitted->SetLineWidth(2);
+    
+    // Adjust font sizes for ratio plot
+    ratio_fitted->GetXaxis()->SetTitleSize(0.12);
+    ratio_fitted->GetXaxis()->SetLabelSize(0.10);
+    ratio_fitted->GetYaxis()->SetTitleSize(0.12);
+    ratio_fitted->GetYaxis()->SetLabelSize(0.10);
+    ratio_fitted->GetYaxis()->SetTitleOffset(0.6);
+    ratio_fitted->GetXaxis()->SetTitleOffset(1.2);
+    ratio_fitted->GetYaxis()->SetNdivisions(505);
+    
+    // Set fonts for ratio histogram
+    SetTH1Fonts(ratio_fitted);
+    
+    // Draw ratio plot
+    ratio_fitted->Draw("lpe");
+    
+    // Add horizontal line at 1.0 in red
+    TLine* line = new TLine(0, 1.0, 600, 1.0);
+    line->SetLineColor(kRed);
+    line->SetLineWidth(2);
+    line->SetLineStyle(2);
+    line->Draw();
+    
     c1->SaveAs("shifted_hfe_comparison.pdf");
 
     fInput->Close();
