e99f3c417bc1f4dfb3cbba3e762185f9e357d2bd
Abraham Holtermann <ath8@mit.edu>
2025-09-03 23:53:43 +0200

    plotting updates :)

--- npartplot/plot2.C
+++ npartplot/plot2.C
@@ -9,88 +9,94 @@ void plot2(){
     TDirectoryFile* dir = (TDirectoryFile*)f->Get("Table 3");
     TH1D*bins = (TH1D*)dir->Get("Hist1D_y1");
 
-    TFile*f2 = TFile::Open("data/c30_50.root");
-    TDirectoryFile* dir2 = (TDirectoryFile*)f2->Get("Table 11");
+    TFile*f2 = TFile::Open("data/c70_90.root");
+    TDirectoryFile* dir2 = (TDirectoryFile*)f2->Get("Table 13");
     TH1D*bins2 = (TH1D*)dir2->Get("Hist1D_y1");
 
     vector<float> smallsystembinedges = {4.8,5.6,6.4,7.2,9.6,12,14.4,19.2,24.0,28.8,35.2,48.0,73.6,103.6};
 
-    TGraphErrors* gOO = new TGraphErrors();
+    TGraphAsymmErrors* gOO = new TGraphAsymmErrors();
     vector<TBox*> boxesOO;
     for(int i = 0; i< 13; i++){
-        GraphEntry entry = getSmallSystemEntry(i, 1);
+        GraphEntry entry = getSmallSystemSystematic_only(i, 1);
+        GraphEntry entry2 = getSmallSystemStatistical(i, 1);
         // Use template histogram for bin width
         double x = (smallsystembinedges[i]+smallsystembinedges[i+1])/2.0;
         cout << x << endl;
         double y = entry.y;
         gOO->SetPoint(i, x, y);
-        gOO->SetPointError(i, 0,0);
+        gOO->SetPointError(i, 0, 0, entry2.eyl, entry2.eyh);
 
         int binIdx = bins->FindBin(x);
         double ex = bins->GetBinWidth(binIdx) / 2.0;
         TBox* box = new TBox(x-ex, y-entry.eyl, x+ex, y+entry.eyh);
-        box->SetFillColorAlpha(cmsBlue,0.35);
+        box->SetFillColorAlpha(cmsBlue,0.19);
         box->SetLineColor(cmsBlue+2);
-    box->SetLineWidth(1);
-    box->SetLineStyle(1); // solid line
+        box->SetLineWidth(1);
+        box->SetLineStyle(1); // solid line
         boxesOO.push_back(box);
     }
 
-    TGraphErrors* gNeNe = new TGraphErrors();
+    TGraphAsymmErrors* gNeNe = new TGraphAsymmErrors();
     vector<TBox*> boxesNeNe;
     for(int i = 0; i< 13; i++){
-        GraphEntry entry = getSmallSystemEntry(i, 2);
+        GraphEntry entry = getSmallSystemSystematic_only(i, 2);
+        GraphEntry entry2 = getSmallSystemStatistical(i, 2);
         // Use template histogram for bin width
         double x = (smallsystembinedges[i]+smallsystembinedges[i+1])/2.0;
         cout << x << endl;
         double y = entry.y;
         gNeNe->SetPoint(i, x, y);
-        gNeNe->SetPointError(i, 0,0);
+        gNeNe->SetPointError(i, 0, 0, entry2.eyl, entry2.eyh);
 
         int binIdx = bins->FindBin(x);
         double ex = bins->GetBinWidth(binIdx) / 2.0;
         TBox* box = new TBox(x-ex, y-entry.eyl, x+ex, y+entry.eyh);
-        box->SetFillColorAlpha(cmsViolet,0.35);
+        box->SetFillColorAlpha(cmsViolet,0.19);
         box->SetLineColor(cmsViolet+2);
         box->SetLineWidth(1);
         box->SetLineStyle(1);
         boxesNeNe.push_back(box);
     }
 
-    TGraphErrors* gXeXe = new TGraphErrors();
+    TGraphAsymmErrors* gXeXe = new TGraphAsymmErrors();
     vector<TBox*> boxesXeXe;
     for(int i = 0; i< 29; i++){
-        GraphEntry entry = getXeXeEntry(i, 5);
+        GraphEntry entry = getXeXeSystematic_only(i, 5);
+        GraphEntry entry2 = getXeXeStatistical(i, 5);
+        if(entry.y < 0.05){continue;}
         gXeXe->SetPoint(i, entry.x, entry.y);
-        gXeXe->SetPointError(i, 0,0);
-    double x = entry.x;
-    if (x < 0.4) continue;
-    double y = entry.y;
-     int binIdx = bins->FindBin(x);
+        gXeXe->SetPointError(i, 0, 0, entry2.eyl, entry2.eyh);
+        double x = entry.x;
+        if (x < 0.4) continue;
+        double y = entry.y;
+        int binIdx = bins->FindBin(x);
         double ex = bins->GetBinWidth(binIdx) / 2.0;
-    TBox* box = new TBox(x-ex, y-entry.eyl, x+ex, y+entry.eyh);
-        box->SetFillColorAlpha(cmsRed,0.35);
+
+        TBox* box = new TBox(x-ex, y-entry.eyl, x+ex, y+entry.eyh);
+        box->SetFillColorAlpha(cmsRed,0.19);
         box->SetLineColor(cmsRed+2);
-    box->SetLineWidth(1);
-    box->SetLineStyle(1);
+        box->SetLineWidth(1);
+        box->SetLineStyle(1);
         boxesXeXe.push_back(box);
     }
 
-    TGraphErrors* gPbPb = new TGraphErrors();
+    TGraphAsymmErrors* gPbPb = new TGraphAsymmErrors();
     vector<TBox*> boxesPbPb;
     for(int i = 0; i< 29; i++){
-        GraphEntry entry = getPbPbEntry(i, 5);
+        GraphEntry entry = getPbPbSystematic_only(i, 5);
+        GraphEntry entry2 = getPbPbStatistical(i, 5);
         gPbPb->SetPoint(i, entry.x, entry.y);
-        gPbPb->SetPointError(i, 0,0);
-    double x = entry.x;
-    double y = entry.y;
-    int binIdx = bins2->FindBin(x);
-    double ex = bins2->GetBinWidth(binIdx) / 2.0;
-    TBox* box = new TBox(x-ex, y-entry.eyl, x+ex, y+entry.eyh);
-        box->SetFillColorAlpha(cmsYellow,0.35);
+        gPbPb->SetPointError(i, 0, 0, entry2.eyl, entry2.eyh);
+        double x = entry.x;
+        double y = entry.y;
+        int binIdx = bins2->FindBin(x);
+        double ex = bins2->GetBinWidth(binIdx) / 2.0;
+        TBox* box = new TBox(x-ex, y-entry.eyl, x+ex, y+entry.eyh);
+        box->SetFillColorAlpha(cmsYellow,0.19);
         box->SetLineColor(cmsYellow+2);
-    box->SetLineWidth(1);
-    box->SetLineStyle(1);
+        box->SetLineWidth(1);
+        box->SetLineStyle(1);
         boxesPbPb.push_back(box);
     }
 
@@ -116,6 +122,8 @@ void plot2(){
     gOO->GetYaxis()->CenterTitle(true);
     gOO->GetXaxis()->SetTitleOffset(1.2);
     gOO->GetYaxis()->SetTitleOffset(1.2);
+    gOO->GetXaxis()->SetTitleSize(0.052);
+    gOO->GetYaxis()->SetTitleSize(0.052);
 
     gNeNe->GetXaxis()->SetTitle(xLabel);
     gNeNe->GetYaxis()->SetTitle(yLabel);
@@ -125,6 +133,8 @@ void plot2(){
     gNeNe->GetYaxis()->CenterTitle(true);
     gNeNe->GetXaxis()->SetTitleOffset(1.2);
     gNeNe->GetYaxis()->SetTitleOffset(1.2);
+    gNeNe->GetXaxis()->SetTitleSize(0.052);
+    gNeNe->GetYaxis()->SetTitleSize(0.052);
 
     gXeXe->GetXaxis()->SetTitle(xLabel);
     gXeXe->GetYaxis()->SetTitle(yLabel);
@@ -134,6 +144,8 @@ void plot2(){
     gXeXe->GetYaxis()->CenterTitle(true);
     gXeXe->GetXaxis()->SetTitleOffset(1.2);
     gXeXe->GetYaxis()->SetTitleOffset(1.2);
+    gXeXe->GetXaxis()->SetTitleSize(0.052);
+    gXeXe->GetYaxis()->SetTitleSize(0.052);
 
     gPbPb->GetXaxis()->SetTitle(xLabel);
     gPbPb->GetYaxis()->SetTitle(yLabel);
@@ -143,31 +155,45 @@ void plot2(){
     gPbPb->GetYaxis()->CenterTitle(true);
     gPbPb->GetXaxis()->SetTitleOffset(1.2);
     gPbPb->GetYaxis()->SetTitleOffset(1.2);
+    gPbPb->GetXaxis()->SetTitleSize(0.052);
+    gPbPb->GetYaxis()->SetTitleSize(0.052);
 
-    TCanvas*c = new TCanvas();
+    TCanvas*c = new TCanvas("","", 900, 900);
     c->cd();
     c->SetLogx();
+    c->SetLeftMargin(0.15);
+    c->SetBottomMargin(0.15);
+    c->SetTopMargin(0.08);
+    c->SetRightMargin(0.05);
 
     // Draw the graphs and boxes
     
     gOO->Draw("AP");
     gOO->SetMarkerColor(cmsBlue);
+    gOO->SetLineColor(cmsBlue);
     gOO->SetMarkerStyle(33);
+    gOO->SetMarkerSize(1.5);
     for (auto box : boxesOO) box->Draw();
 
     gNeNe->Draw("P SAME");
     gNeNe->SetMarkerColor(cmsViolet);
+    gNeNe->SetLineColor(cmsViolet);
     gNeNe->SetMarkerStyle(33);
+    gNeNe->SetMarkerSize(1.5);
     for (auto box : boxesNeNe) box->Draw();
 
     gXeXe->Draw("P SAME");
     gXeXe->SetMarkerColor(cmsRed);
+    gXeXe->SetLineColor(cmsRed);
     gXeXe->SetMarkerStyle(33);
+    gXeXe->SetMarkerSize(1.5);
     for (auto box : boxesXeXe) box->Draw();
 
     gPbPb->Draw("P SAME");
     gPbPb->SetMarkerColor(cmsYellow);
+    gPbPb->SetLineColor(cmsYellow);
     gPbPb->SetMarkerStyle(33);
+    gPbPb->SetMarkerSize(1.5);
     for (auto box : boxesPbPb) box->Draw();
 
     // Draw horizontal dotted line at y=1
@@ -178,62 +204,97 @@ void plot2(){
     line->Draw();
 
     TH1D*hOO = new TH1D("h", "Histogram", 100, 0, 200);
-    hOO->SetFillColorAlpha(cmsBlue, 0.35);
+    hOO->SetFillColorAlpha(cmsBlue, 0.19);
     hOO->SetMarkerStyle(33);
     hOO->SetMarkerColor(cmsBlue);
+    hOO->SetMarkerSize(1.5);
     hOO->SetFillStyle(1001);
     hOO->SetLineWidth(0);
     hOO->Draw("SAME");
 
     TH1D*hNeNe = new TH1D("h", "Histogram", 100, 0, 200);
-    hNeNe->SetFillColorAlpha(cmsViolet, 0.35);
+    hNeNe->SetFillColorAlpha(cmsViolet, 0.19);
     hNeNe->SetMarkerStyle(33);
     hNeNe->SetMarkerColor(cmsViolet);
+    hNeNe->SetMarkerSize(1.5);
     hNeNe->SetFillStyle(1001);
     hNeNe->SetLineWidth(0);
     hNeNe->Draw("SAME");
 
     TH1D*hXeXe = new TH1D("h", "Histogram", 100, 0, 200);
-    hXeXe->SetFillColorAlpha(cmsRed, 0.35);
+    hXeXe->SetFillColorAlpha(cmsRed, 0.19);
     hXeXe->SetMarkerStyle(33);
     hXeXe->SetMarkerColor(cmsRed);
+    hXeXe->SetMarkerSize(1.5);
     hXeXe->SetFillStyle(1001);
     hXeXe->SetLineWidth(0);
     hXeXe->Draw("SAME");
 
     TH1D*hPbPb = new TH1D("h", "Histogram", 100, 0, 200);
-    hPbPb->SetFillColorAlpha(cmsYellow, 0.35);
+    hPbPb->SetFillColorAlpha(cmsYellow, 0.19);
     hPbPb->SetMarkerStyle(33);
     hPbPb->SetMarkerColor(cmsYellow);
+    hPbPb->SetMarkerSize(1.5);
     hPbPb->SetFillStyle(1001);
     hPbPb->SetLineWidth(0);
     hPbPb->Draw("SAME");
 
-    TLegend* L = new TLegend(0.5, 0.65, 0.89, 0.89);
-    L->AddEntry(hOO, "OO 5.02 TeV:   #LT N_{Part} #GT = 10.8", "pf");
-    L->AddEntry(hNeNe, "NeNe 5.02 TeV:   #LT N_{Part} #GT = 12.7", "pf");
-    L->AddEntry(hXeXe, "XeXe 5.44 TeV (70 - 80%):    #LT N_{Part} #GT = 10.55", "pf");
-    L->AddEntry(hPbPb, "PbPb 5.02 TeV (70 - 90%):    #LT N_{Part} #GT = 11.1", "pf");
+    TLegend* L = new TLegend(0.32, 0.65, 0.89, 0.85);
+    L->SetTextSize(0.023);
+    L->AddEntry(hPbPb, "PbPb (5.02 TeV) 70 - 90%, 404  #mub^{-1}  #LT N_{Part} #GT = 11.1", "pf");
+    L->AddEntry(hXeXe, "XeXe (5.44 TeV) 70 - 80%, 3.42  #mub^{-1} #LT N_{Part} #GT = 10.55", "pf");
+    L->AddEntry(hNeNe, "NeNe (5.36 TeV) 0 - 100%, 0.8 nb^{-1}    #LT N_{Part} #GT = 12.7", "pf");
+    L->AddEntry(hOO,   "OO    (5.36 TeV) 0 - 100%, 6.0 nb^{-1}    #LT N_{Part} #GT = 10.8", "pf");
     L->SetBorderSize(0);
     L->SetFillStyle(0);
     L->Draw();
 
      // Add TLatex label for pseudorapidity
-    TLatex* latex_eta = new TLatex(4, 1.5, "|#eta| < 1");
-    latex_eta->SetTextSize(0.03);
+    TLatex* latex_eta = new TLatex(0.55, 1.35, "Track |#eta| < 1");
+    latex_eta->SetTextSize(0.027);
     latex_eta->SetTextFont(42);
     latex_eta->Draw();
 
 
+    float XeXeLumi = 0.26;
+    float PbPbLumi = 0.16;
+    float NeNeLumi = 0.05;
+    float OOLumi = 0.05;
 
+    TBox* box_PbPblumi = new TBox(0.5, 1-PbPbLumi, 0.55, 1+PbPbLumi);
+    box_PbPblumi->SetFillStyle(0);
+    box_PbPblumi->SetLineColor(cmsYellow);
+    box_PbPblumi->SetLineWidth(2);
     
+    TBox* box_XeXelumi = new TBox(0.55, 1-XeXeLumi, 0.605, 1+XeXeLumi);
+    box_XeXelumi->SetLineColor(cmsRed);
+    box_XeXelumi->SetFillStyle(0);
+    box_XeXelumi->SetLineWidth(2);
+
+    TBox* box_NeNelumi = new TBox(0.605, 1-NeNeLumi, 0.6655, 1+NeNeLumi);
+    box_NeNelumi->SetLineColor(cmsViolet);
+    box_NeNelumi->SetFillStyle(0);
+    box_NeNelumi->SetLineWidth(2);
+
+    TBox* box_OOLumi = new TBox(0.6655, 1 - OOLumi, 0.73205, 1 + OOLumi);
+    box_OOLumi->SetLineColor(cmsBlue);
+    box_OOLumi->SetFillStyle(0);
+    box_OOLumi->SetLineWidth(2);
+
+    box_PbPblumi->Draw();
+    box_XeXelumi->Draw();
+    box_NeNelumi->Draw();
+    box_OOLumi->Draw();
+
 
     gPad->Update();
-    gPad->GetFrame()->SetX1(xMin);
-    gPad->GetFrame()->SetX2(xMax);
-    gPad->GetFrame()->SetY1(yMin);
-    gPad->GetFrame()->SetY2(yMax);
-    gPad->Update();
+    // Remove these frame calls that override margins
+    // gPad->GetFrame()->SetX1(xMin);
+    // gPad->GetFrame()->SetX2(xMax);
+    // gPad->GetFrame()->SetY1(yMin);
+    // gPad->GetFrame()->SetY2(yMax);
+    // gPad->Update();
+    AddCMSHeader(c, true);
 
     c->SaveAs("plot2.pdf");
 }
--- npartplot/scrape.C
+++ npartplot/scrape.C
@@ -11,13 +11,11 @@ struct GraphEntry {
     double eyl;
 };
 
-GraphEntry getSmallSystemEntry(int ptBin, int index) {
-    //std::string centval = "gRAA_PtVsNpart_ptBin" + std::to_string(ptBin);
-    std::string centval = "gRAA_PtVsNpart_TotUncert_ptBin" + std::to_string(ptBin);
+GraphEntry getSmallSystemStatistical(int ptBin, int index){
+    std::string centval = "gRAA_PtVsNpart_Stat_ptBin" + std::to_string(ptBin);
     TFile* file = TFile::Open("data/ptBinned_RAAVsNpart.root");
-
     TGraphErrors* graph = (TGraphErrors*)file->Get(centval.c_str());
-    
+
     if (index < 0 || index >= graph->GetN()) {
         file->Close();
         throw std::out_of_range("Index out of range.");
@@ -27,16 +25,119 @@ GraphEntry getSmallSystemEntry(int ptBin, int index) {
     double ex = graph->GetErrorX(index);
     double ey = graph->GetErrorY(index);
     file->Close();
+    return {x, y, ey, ey};
+}
+
+GraphEntry getSmallSystemSystematic(int ptBin, int index) {
+    //std::string centval = "gRAA_PtVsNpart_ptBin" + std::to_string(ptBin);
+    //std::string centval = "gRAA_PtVsNpart_TotUncert_ptBin" + std::to_string(ptBin);
+    std::string centval1 = "gRAA_PtVsNpart_Syst_ptBin" + std::to_string(ptBin);
+    std::string centval2 = "gRAA_PtVsNpart_Norm_ptBin" + std::to_string(ptBin);
+    TFile* file = TFile::Open("data/ptBinned_RAAVsNpart.root");
+
+    TGraphErrors* graph1 = (TGraphErrors*)file->Get(centval1.c_str());
+    TGraphErrors* graph2 = (TGraphErrors*)file->Get(centval2.c_str());
+
+    if (index < 0 || index >= graph1->GetN()) {
+        file->Close();
+        throw std::out_of_range("Index out of range.");
+    }
+    double x, y;
+    graph1->GetPoint(index, x, y);
+    double ex = graph1->GetErrorX(index);
+    double ey = sqrt(graph1->GetErrorY(index) * graph1->GetErrorY(index) + graph2->GetErrorY(index) * graph2->GetErrorY(index));
+    file->Close();
     //cout << "success" << endl;
     return {x, y, ey, ey};
 }
 
+GraphEntry getSmallSystemSystematic_only(int ptBin, int index) {
+    //std::string centval = "gRAA_PtVsNpart_ptBin" + std::to_string(ptBin);
+    //std::string centval = "gRAA_PtVsNpart_TotUncert_ptBin" + std::to_string(ptBin);
+    std::string centval1 = "gRAA_PtVsNpart_Syst_ptBin" + std::to_string(ptBin);
+    std::string centval2 = "gRAA_PtVsNpart_Norm_ptBin" + std::to_string(ptBin);
+    TFile* file = TFile::Open("data/ptBinned_RAAVsNpart.root");
+
+    TGraphErrors* graph1 = (TGraphErrors*)file->Get(centval1.c_str());
+    TGraphErrors* graph2 = (TGraphErrors*)file->Get(centval2.c_str());
+
+    if (index < 0 || index >= graph1->GetN()) {
+        file->Close();
+        throw std::out_of_range("Index out of range.");
+    }
+    double x, y;
+    graph1->GetPoint(index, x, y);
+    double ex = graph1->GetErrorX(index);
+    double ey = sqrt(graph1->GetErrorY(index) * graph1->GetErrorY(index));
+    file->Close();
+    //cout << "success" << endl;
+    return {x, y, ey, ey};
+}
+
+GraphEntry getXeXeStatistical(int ptBin, int centbin) {
+
+    TFile* F = TFile::Open("data/XeXeDifferential.root");
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 3");
+    TH1F* graph = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1)).c_str());
+    TH1F* graph1 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e1").c_str());
+    TH1F* graph2 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e2").c_str());
+    TH1F* graph3 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e3").c_str());
+
+
+    double x, y;
+    x = graph->GetBinCenter(ptBin);
+    y = graph->GetBinContent(ptBin);
+    //double ex = graph->GetErrorX(ptBin);
+    double eyl = graph1->GetBinError(ptBin);
+    double eyh = graph1->GetBinError(ptBin);
+    F->Close();
+    return {x, y, eyl, eyh};
+}
+
+GraphEntry getXeXeSystematic(int ptBin, int centbin) {
+
+    TFile* F = TFile::Open("data/XeXeDifferential.root");
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 3");
+    TH1F* graph = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1)).c_str());
+    TH1F* graph1 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e1").c_str());
+    TH1F* graph2 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e2").c_str());
+    TH1F* graph3 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e3").c_str());
+
+    double x, y;
+    x = graph->GetBinCenter(ptBin);
+    y = graph->GetBinContent(ptBin);
+    //double ex = graph->GetErrorX(ptBin);
+    double eyl = sqrt(graph2->GetBinError(ptBin)*graph2->GetBinError(ptBin) + graph3->GetBinError(ptBin)*graph3->GetBinError(ptBin));
+    double eyh = sqrt(graph2->GetBinError(ptBin)*graph2->GetBinError(ptBin) + graph3->GetBinError(ptBin)*graph3->GetBinError(ptBin));
+    F->Close();
+    return {x, y, eyl, eyh};
+}
+
+GraphEntry getXeXeSystematic_only(int ptBin, int centbin){
+
+    TFile* F = TFile::Open("data/XeXeDifferential.root");
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 3");
+    TH1F* graph = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1)).c_str());
+    TH1F* graph1 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e1").c_str());
+    TH1F* graph2 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e2").c_str());
+    TH1F* graph3 = (TH1F*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1) + "_e3").c_str());
+
+    double x, y;
+    x = graph->GetBinCenter(ptBin);
+    y = graph->GetBinContent(ptBin);
+    //double ex = graph->GetErrorX(ptBin);
+    double eyl = sqrt(graph2->GetBinError(ptBin)*graph2->GetBinError(ptBin));
+    double eyh = sqrt(graph2->GetBinError(ptBin)*graph2->GetBinError(ptBin));
+    F->Close();
+    return {x, y, eyl, eyh};
+
+}
 
 GraphEntry getXeXeEntry(int ptBin, int centbin) {
 
     TFile* F = TFile::Open("data/XeXeDifferential.root");
     TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 3");
-    TGraphAsymmErrors* graph = (TGraphAsymmErrors*)dir->Get(("Graph1D_y" + std::to_string(centbin + 1)).c_str());
+    TGraphAsymmErrors* graph = (TGraphAsymmErrors*)dir->Get(("Hist1D_y" + std::to_string(centbin + 1)).c_str());
     //cout << ("Graph1D_y" + std::to_string(centbin)).c_str() << endl;
 
     double x, y;
@@ -47,6 +148,95 @@ GraphEntry getXeXeEntry(int ptBin, int centbin) {
     F->Close();
     return {x, y, eyl, eyh};
 }
+
+GraphEntry getPbPbStatistical(int ptBin, int centbin) {
+
+    vector<const char*> filenames = {
+        "data/c0_5.root",
+        "data/c5_10.root",
+        "data/c10_30.root",
+        "data/c30_50.root",
+        "data/c50_70.root",
+        "data/c70_90.root"
+    };
+
+    TFile* F = TFile::Open(filenames[centbin]);
+    std::string dirName = "Table " + std::to_string(centbin + 8);
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get(dirName.c_str());
+    TH1F* hist = (TH1F*)dir->Get("Hist1D_y1_e1");
+    TH1F* histc = (TH1F*)dir->Get("Hist1D_y1");
+
+    double x, y;
+    //hist->GetPoint(ptBin, x, y);
+    x = histc->GetBinCenter(ptBin);
+    y = histc->GetBinContent(ptBin);
+    //double ex = hist->GetErrorX(ptBin);
+    double eyl = hist->GetBinError(ptBin);
+    double eyh = hist->GetBinError(ptBin);
+    F->Close();
+    return {x, y, eyl, eyh};
+}
+
+GraphEntry getPbPbSystematic(int ptBin, int centbin) {
+
+    vector<const char*> filenames = {
+        "data/c0_5.root",
+        "data/c5_10.root",
+        "data/c10_30.root",
+        "data/c30_50.root",
+        "data/c50_70.root",
+        "data/c70_90.root"
+    };
+
+    TFile* F = TFile::Open(filenames[centbin]);
+    std::string dirName = "Table " + std::to_string(centbin + 8);
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get(dirName.c_str());
+    TH1F* hist1 = (TH1F*)dir->Get("Hist1D_y1");
+    TH1F* hist2 = (TH1F*)dir->Get("Hist1D_y1_e2");
+    TH1F* hist3 = (TH1F*)dir->Get("Hist1D_y1_e3plus");
+    TH1F* hist4 = (TH1F*)dir->Get("Hist1D_y1_e3minus");
+    TH1F* hist5 = (TH1F*)dir->Get("Hist1D_y1_e4");
+
+    double x, y;
+    x = hist1->GetBinCenter(ptBin);
+    y = hist1->GetBinContent(ptBin);
+    //double ex = hist->GetErrorX(ptBin);
+    double eyh = sqrt(hist2->GetBinError(ptBin)*hist2->GetBinError(ptBin) + hist3->GetBinError(ptBin)*hist3->GetBinError(ptBin) + hist5->GetBinError(ptBin)*hist5->GetBinError(ptBin));
+    double eyl = sqrt(hist2->GetBinError(ptBin)*hist2->GetBinError(ptBin) + hist4->GetBinError(ptBin)*hist4->GetBinError(ptBin) + hist5->GetBinError(ptBin)*hist5->GetBinError(ptBin));
+    F->Close();
+    return {x, y, eyl, eyh};
+}
+
+GraphEntry getPbPbSystematic_only(int ptBin, int centbin) {
+
+    vector<const char*> filenames = {
+        "data/c0_5.root",
+        "data/c5_10.root",
+        "data/c10_30.root",
+        "data/c30_50.root",
+        "data/c50_70.root",
+        "data/c70_90.root"
+    };
+
+    TFile* F = TFile::Open(filenames[centbin]);
+    std::string dirName = "Table " + std::to_string(centbin + 8);
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get(dirName.c_str());
+    TH1F* hist1 = (TH1F*)dir->Get("Hist1D_y1");
+    TH1F* hist2 = (TH1F*)dir->Get("Hist1D_y1_e2");
+    TH1F* hist3 = (TH1F*)dir->Get("Hist1D_y1_e3plus");
+    TH1F* hist4 = (TH1F*)dir->Get("Hist1D_y1_e3minus");
+    TH1F* hist5 = (TH1F*)dir->Get("Hist1D_y1_e4");
+
+    double x, y;
+    x = hist1->GetBinCenter(ptBin);
+    y = hist1->GetBinContent(ptBin);
+    //double ex = hist->GetErrorX(ptBin);
+    double eyh = sqrt(hist2->GetBinError(ptBin)*hist2->GetBinError(ptBin));
+    double eyl = sqrt(hist2->GetBinError(ptBin)*hist2->GetBinError(ptBin));
+    F->Close();
+    return {x, y, eyl, eyh};
+}
+
 
 GraphEntry getPbPbEntry(int ptBin, int centbin) {
 
@@ -71,6 +261,67 @@ GraphEntry getPbPbEntry(int ptBin, int centbin) {
     double eyh = graph->GetErrorYhigh(ptBin);
     F->Close();
     return {x, y, eyl, eyh};
+}
+
+GraphEntry getpPbStatistical(int ptBin){
+
+    TFile*F = TFile::Open("data/pPb.root");
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 16");
+    TH1F* hist = (TH1F*)dir->Get("Hist1D_y1");
+    TH1F* hist1 = (TH1F*)dir->Get("Hist1D_y1_e1");
+
+    double x, y;
+    x = hist->GetBinCenter(ptBin);
+    y = hist->GetBinContent(ptBin);
+    double ex = hist->GetBinError(ptBin);
+    double eyl = hist1->GetBinError(ptBin);
+    double eyh = hist1->GetBinError(ptBin);
+    F->Close();
+    return {x, y, eyl, eyh};
+}
+
+GraphEntry getpPbSystematic(int ptBin){
+
+    TFile*F = TFile::Open("data/pPb.root");
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 16");
+    TH1F* hist = (TH1F*)dir->Get("Hist1D_y1");
+    TH1F* hist2 = (TH1F*)dir->Get("Hist1D_y1_e2plus");
+    TH1F* hist3 = (TH1F*)dir->Get("Hist1D_y1_e2minus");
+    TH1F* hist4 = (TH1F*)dir->Get("Hist1D_y1_e3plus");
+    TH1F* hist5 = (TH1F*)dir->Get("Hist1D_y1_e3minus");
+    TH1F* hist6 = (TH1F*)dir->Get("Hist1D_y1_e4");
+
+    double x, y;
+    x = hist->GetBinCenter(ptBin);
+    y = hist->GetBinContent(ptBin);
+    double ex = hist->GetBinError(ptBin);
+    double eyh = sqrt(hist2->GetBinError(ptBin)*hist2->GetBinError(ptBin) + hist4->GetBinError(ptBin)*hist4->GetBinError(ptBin) + hist6->GetBinError(ptBin)*hist6->GetBinError(ptBin));
+    double eyl = sqrt(hist3->GetBinError(ptBin)*hist3->GetBinError(ptBin) + hist5->GetBinError(ptBin)*hist5->GetBinError(ptBin) + hist6->GetBinError(ptBin)*hist6->GetBinError(ptBin));
+    F->Close();
+    return {x, y, eyl, eyh};
+
+}
+
+GraphEntry getpPbSystematic_only(int ptBin){
+
+    TFile*F = TFile::Open("data/pPb.root");
+    TDirectoryFile* dir = (TDirectoryFile*)F->Get("Table 16");
+    TH1F* hist = (TH1F*)dir->Get("Hist1D_y1");
+    TH1F* hist2 = (TH1F*)dir->Get("Hist1D_y1_e2plus");
+    TH1F* hist3 = (TH1F*)dir->Get("Hist1D_y1_e2minus");
+    TH1F* hist4 = (TH1F*)dir->Get("Hist1D_y1_e3plus");
+    TH1F* hist5 = (TH1F*)dir->Get("Hist1D_y1_e3minus");
+    TH1F* hist6 = (TH1F*)dir->Get("Hist1D_y1_e4");
+
+    double x, y;
+    x = hist->GetBinCenter(ptBin);
+    y = hist->GetBinContent(ptBin);
+    double ex = hist->GetBinError(ptBin);
+    double eyh = sqrt(hist2->GetBinError(ptBin)*hist2->GetBinError(ptBin));
+    double eyl = sqrt(hist3->GetBinError(ptBin)*hist3->GetBinError(ptBin));
+    F->Close();
+    return {x, y, eyl, eyh};
+
 }
 
 GraphEntry getPbPb_Inclusive(int ptBin){
