e79f1fb2e6b7b9c35a6d58e53565d5c4a4e13015
Jordan Lang <jordandlang.sci@gmail.com>
2025-10-21 00:00:44 +0200

    Renamed Dalpha, Ddtheta systematic cuts to be clearer. Removed branch checks since Dzero PAS state is now a tag.

--- CommonCode/include/Messenger.h
+++ CommonCode/include/Messenger.h
@@ -608,7 +608,7 @@ public:
    std::vector<bool> *SingleMuIsGlobal;
    std::vector<bool> *SingleMuIsTracker;
    std::vector<bool> *SingleMuHybridSoft;
-   
+
    std::vector<float> *GenSingleMuPT;
    std::vector<float> *GenSingleMuEta;
    std::vector<float> *GenSingleMuPhi;
