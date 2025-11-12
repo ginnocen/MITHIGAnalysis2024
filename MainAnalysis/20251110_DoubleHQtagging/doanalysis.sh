443da89f1b5efeac3579d9e43b9bb314a79ccba6
Abraham Holtermann <ath8@mit.edu>
2025-11-12 22:43:08 +0100

    small tweak to submissio nscripts

--- MainAnalysis/20251110_DoubleHQtagging/doanalysis.sh
+++ MainAnalysis/20251110_DoubleHQtagging/doanalysis.sh
@@ -1,6 +1,6 @@
 source clean.sh
 rm *.root
-INPUT=/data00/g2ccbar/mc2018/skim_102725_0/mergedfile.root
+INPUT=/data00/g2ccbar/mc2018/skim_102725_all/mergedfile.root
 ./ExecuteDoubleHQtagging \
      --Input $INPUT \
      --IsData 0 \
@@ -8,8 +8,6 @@ INPUT=/data00/g2ccbar/mc2018/skim_102725_0/mergedfile.root
      --MinJetPT 80 \
      --MaxJetPT 120 \
      --ChargeSelection -1 \
-     --NbHad -1 \
-     --NcHad -1 \
      --DCAString "abs(muDiDxy1) > 0.005 && abs(muDiDxy2) > 0.005" \
      --Output output_DoubleHQtagging.root
 
